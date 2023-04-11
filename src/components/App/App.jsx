1c8358bad1d3502685c98899ee53512af8c2ecf4
Taisiya Tomashevska <taya22011985@gmail.com>
2023-04-11 07:41:51 +0300

    add check for contacts.length

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,24 @@
-
+import { useSelector } from 'react-redux';
 import { Form } from '../Form/Form';
 import { ContactList } from '../ContactList/ContactList';
 import { Filter } from '../Filter/Filter';
 import { Section } from 'components/Section/Section';
 import css from './App.module.css';
+import { getContact } from 'redux/selectors';
 
 export const App = () => {
+  const contacts = useSelector(getContact);
   return (
     <div className={css.wrapper}>
-      <Section title='Phone book'>
+      <Section title="Phone book">
         <Form />
       </Section>
-      <Section title='Contacts'>
-        <Filter />
-        <ContactList />
-      </Section>
+      {contacts.length > 0 && (
+        <Section title="Contacts">
+          <Filter />
+          <ContactList />
+        </Section>
+      )}
     </div>
   );
 };
